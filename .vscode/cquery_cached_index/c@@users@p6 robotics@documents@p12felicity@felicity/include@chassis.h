748f898f2da5287edab5899ab84c7f2166951444
neisaah2742 <neisaah2742@gmail.com>
2023-01-26 15:20:10 -0800

    ...

--- .vscode/cquery_cached_index/c@@users@p6 robotics@documents@p12felicity@felicity/include@chassis.h
+++ .vscode/cquery_cached_index/c@@users@p6 robotics@documents@p12felicity@felicity/include@chassis.h
@@ -3,6 +3,13 @@
 #define _CHASSIS_H_
 
 //sets speeds of motors
-//void chassisSet(int left, int right);
+void chassisSet(int left, int right) {
+    motorSet(2, left); //frontLeft
+    motorSet(3, left); //backLeft
+    motorSet(4, right); //backRight
+    motorSet(5, right); //frontRight
+    printf("right speed %d left speed %d \n", right, left);
+}
+
 
 #endif // _CHASSIS_H_
--- .vscode/cquery_cached_index/c@@users@p6 robotics@documents@p12felicity@felicity/src@chassis.c
+++ .vscode/cquery_cached_index/c@@users@p6 robotics@documents@p12felicity@felicity/src@chassis.c
@@ -2,10 +2,10 @@
 #include "main.h"
 #include "chassis.h"
 
-void chassisSet(int left, int right) {
+/*void chassisSet(int left, int right) {
     motorSet(2, left); //frontLeft
     motorSet(3, left); //backLeft
     motorSet(4, right); //backRight
     motorSet(5, right); //frontRight
     printf("right speed %d left speed %d \n", right, left);
-}
+}*/
